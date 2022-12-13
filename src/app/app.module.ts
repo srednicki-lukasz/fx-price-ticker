0ce9cccd637f62d3ebb71f1646ea58e62d168575
Łukasz Średnicki <srednickilukasz@gmail.com>
2022-12-13 15:20:55 +0100

    Added angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
+
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,6 +12,7 @@ import { AppComponent } from './app.component';
 	],
 	imports: [
 		BrowserModule,
+		BrowserAnimationsModule,
 		AppRoutingModule
 	],
 	providers: [],
